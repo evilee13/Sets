8132147053bbdd7a0df9e8f10f2e44f7d60bcc19
Elina <e.gabbasova@bftcom.com>
2021-06-24 14:17:21 +0500

    Map

--- src/main/java/com/bft/com/App.java
+++ src/main/java/com/bft/com/App.java
@@ -1,20 +1,41 @@
 package com.bft.com;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        Person person1 = new Person("Ольга", "Иванова");
-        Person person2 = new Person("Алексей", "Петров");
-        Person person3 = new Person("Ольга", "Михайлова");
-        Person person4 = new Person("Иван", "Иванов");
-        Person person5 = new Person("Вячеслав", "Антонов");
-        Person person6 = new Person("Александр", "Кузьмин");
-        Person person7 = new Person("Ольга", "Иванова");
+        Person person1 = new Person();
+        person1.setFirstName("Ольга");
+        person1.setLastName("Иванова");
+
+        Person person2 = new Person();
+        person2.setFirstName("Алексей");
+        person2.setLastName("Петров");
+
+        Person person3 = new Person();
+        person3.setFirstName("Ольга");
+        person3.setLastName("Михайлова");
+
+        Person person4 = new Person();
+        person4.setFirstName("Иван");
+        person4.setLastName("Иванов");
+
+        Person person5 = new Person();
+        person5.setFirstName("Вячеслав");
+        person5.setLastName("Антонов");
+
+        Person person6 = new Person();
+        person6.setFirstName("Александр");
+        person6.setLastName("Кузьмин");
+
+        Person person7 = new Person();
+        person7.setFirstName("Ольга");
+        person7.setLastName("Иванова");
 
         System.out.println(person1.equals(person3));
 
-        ArrayList<Person> personArrayList = new ArrayList<>(Arrays.asList(person1, person2, person3, person4, person5, person6, person7));
+        List<Person> personArrayList = new ArrayList<>(Arrays.asList(person1, person2, person3, person4, person5, person6, person7));
 
         HashSet<Person> listOfPerson = new HashSet<>(personArrayList);
 
@@ -31,5 +52,27 @@ public class App {
         for (Person p : linkedList)
             System.out.println(p);
 
+//        Map<String, List<Person>> personMap = personArrayList.stream()
+//                .collect(Collectors.groupingBy(Person::getFirstName));
+//        System.out.println(personMap);
+
+        Map<String, List<Person>> personMap = new HashMap<>();
+
+        for (Person person : personArrayList) {
+            String firstName = person.getFirstName();
+            if (!personMap.containsKey(firstName)) {
+                List<Person> personList = new ArrayList<>();
+                personMap.put(firstName, personList);
+            }
+                personMap.get(firstName).add(person);
+            System.out.println("Мапа ключ-значение:"+" "+ personMap);
+
+        }
+
+        List<Person> getValue = personMap.get("Ольга");
+        System.out.println("Ключ Ольга:" + " " + getValue);
     }
 }
+
+
+
--- src/main/java/com/bft/com/Person.java
+++ src/main/java/com/bft/com/Person.java
@@ -2,14 +2,33 @@ package com.bft.com;
 
 
 public class Person implements Comparable <Person> {
-    String firstName;
-    String lastName;
+    private String firstName;
+    private String lastName;
+
+    public Person(){
+
+    }
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Override
     public boolean equals(Object person) {
